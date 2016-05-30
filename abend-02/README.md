800ca9d70da2ef559ba1bfac3b57e4f338d06dfb
Noel Bellon <nicam@gmx.net>
2016-05-30 21:38:43 +0200

    - first commit

---
+++ abend-02/README.md
@@ -0,0 +1,29 @@
+# 02: HTML/CSS (Teil 1)
+
+* *Datum*: 11. Mai 2016
+* *Dozenten*: [Matthias Meier](https://github.com/matthiasmeier), [Thomas Jaggi](https://github.com/backflip)
+
+Beispiel eines statischen Webservers mit Node als Basis für zukünftige Aufgaben.
+
+## Aufsetzen
+
+```bash
+# Repo in eigenen Account forken (mit GitHub GUI)
+
+# Repo klonen
+git clone https://github.com/USERNAME/02-HTML-CSS.git
+
+# In Verzeichnis wechseln
+cd 02-HTML-CSS
+
+# NPM-Dependencies installieren (in package.json definiert)
+npm i
+```
+
+## Webserver starten
+
+```bash
+node index.js
+```
+
+Im Browser unter http://localhost:3000 erreichbar.
---
+++ abend-03/JS/OPTIONAL_jsDemoTypescript.js
@@ -0,0 +1,58 @@
+/**
+ * Created by mstolze on 16/05/16.
+ */
+var ARRAY1 = [1, 'hallo', true, [2, 4, 7], function () { return 'kkk'; },
+    { name: 'm' }];
+//also global var -- does not work in TS
+//window.ARRY2 = [3, 4, 5];
+var ARRY2 = [3, 4, 5];
+var G3 = ARRAY1.forEach(function (item) { console.log(item); }); // directly executed
+var G4 = "Hallo";
+function fun1(par1) {
+    var a3 = [1, 2, 3];
+    console.log(a3, par1);
+}
+fun1(G3);
+function printIndexable1(indexable) {
+    for (var i = 0; i < indexable.length; i++) {
+        console.log(indexable[i]);
+    }
+}
+printIndexable1(ARRAY1);
+printIndexable1(G4);
+printIndexable1(5);
+function lexicalScopeDemo1(par1) {
+    console.log("entered lexicalScopeDemo1");
+    var localVar1 = 1;
+    var localVar2 = 1;
+    console.log(par1, localVar1);
+    lexicalScopeDemo2(2);
+}
+function lexicalScopeDemo2(par1) {
+    console.log("entered lexicalScopeDemo2");
+    var localVar1 = 2;
+    console.log(par1, localVar1);
+    // followng line error if uncommented; in TS already visible at edit-time
+    console.log(localVar2);
+}
+lexicalScopeDemo1(1);
+function keepConstantValClosureDemo(constant) {
+    return function () { return constant; };
+}
+var CONSTANT5 = keepConstantValClosureDemo(5);
+console.log("CONSTANT5()=", CONSTANT5());
+var OBJ1 = { name: 'markus', age: 53, incAge: function () { this.age += 1; } };
+console.log("OBJ1=", OBJ1);
+OBJ1.incAge();
+console.log("OBJ1=", OBJ1);
+// the following statment ceates an Error in TS
+OBJ1.nnname = "Peter";
+//OBJ1.name="Peter";
+console.log("OBJ1=", OBJ1);
+//the following statement creates an error in TS
+OBJ1.age = "55";
+//OBJ1.age=55;
+console.log("OBJ1=", OBJ1);
+OBJ1.incAge();
+console.log("OBJ1=", OBJ1);
+//# sourceMappingURL=OPTIONAL_jsDemoTypescript.js.map
